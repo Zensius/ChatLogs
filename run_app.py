e0c7425c615518b27f89979fb535e4124f1c6b98
Kelvin Zeng <61463192+Zensius@users.noreply.github.com>
2026-02-10 19:58:38 -0800

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+import random
+import datetime as dt
 
+
+st.set_page_config(page_title= "Connect Dashbord",
+                   page_icon= ":telephone_receiver:",
+                   layout="wide")
